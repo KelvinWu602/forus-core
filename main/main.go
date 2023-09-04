3e15692f828b7b48aa95d9171dbe5463bc5bc426
kelvin <kelvin@kelvin.old.comp>
2023-09-05 00:19:31 +0800

    Work on package 'message'

--- main/main.go
+++ main/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/KelvinWu602/forus-core/message"
+)
+
+func main() {
+	var m message.T2Message
+	m.JobID = 1
+	m.ProxyID = 2
+	m.Salt = 3
+	m.Content[0] = 4
+	msg := m.ToBytes()
+	fmt.Println(msg)
 }
--- message/message.go
+++ message/message.go
@@ -6,18 +6,36 @@ import (
 	"encoding/binary"
 )
 
+type MessageType uint8
+
+const (
+	T1 MessageType = iota
+	T2
+)
+
 // SIZE is the number of bytes of the message before encryption.
 const SIZE uint16 = 512
 
 // METADATA_SIZE is the total number of bytes of the jobID, proxyID and salt.
-const METADATA_SIZE = 72
+const METADATA_SIZE = 5
 
 // CONTENT_SIZE is the total number of bytes of the application message.
 const CONTENT_SIZE = SIZE - METADATA_SIZE
 
+// t1MessageBody is dummy values used to fill up T1Message's first 511 bytes
+var t1MessageBody [SIZE - 1]byte
+
+// initialize T1 message body except salt
+func init() {
+	if _, err := rand.Read(t1MessageBody[:]); err != nil {
+		panic(err)
+	}
+}
+
 // InterNodeMessage is the base type of both Type 1 and Type 2 messages.
 type InterNodeMessage interface {
-	ToBytes() [SIZE]byte // All InterNodeMessage supports a ToBytes method which serializes the message itself.
+	ToBytes() [SIZE]byte  // ToBytes serializes the message struct.
+	FromBytes([SIZE]byte) // FromBytes reads a byte sequence into a message struct.
 }
 
 // T1Message will be sent periodically to other nodes in the same cover group that are in the health set of this node.
@@ -33,24 +51,55 @@ type T2Message struct {
 	Salt    uint8              // Salt is a random odd number.
 }
 
-// ToBytes returns a serialized T1 message that is ready to be encrypted
+// ToBytes returns a serialized T1 message that is ready to be encrypted.
 func (t1 *T1Message) ToBytes() [SIZE]byte {
-	var output [SIZE]byte
-	buffer := make([]byte, SIZE)
-	if _, err := rand.Read(buffer); err != nil {
-		panic(err)
+	if t1.Salt%2 != 0 {
+		panic("Last byte must be even!")
 	}
-	copy(output[:SIZE-1], buffer)
+	var output [SIZE]byte
+	copy(output[:SIZE-1], t1MessageBody[:])
 	output[SIZE-1] = t1.Salt
 	return output
 }
 
-// ToBytes returns a serialized T2 message that is ready to be encrypted
+// ToBytes returns a serialized T2 message that is ready to be encrypted.
 func (t2 *T2Message) ToBytes() [SIZE]byte {
+	if t2.Salt%2 != 1 {
+		panic("Last byte must be odd!")
+	}
 	var output [SIZE]byte
 	binary.BigEndian.PutUint32(output[0:4], uint32(t2.JobID))
 	binary.BigEndian.PutUint32(output[4:8], uint32(t2.ProxyID))
 	copy(output[8:SIZE-2], t2.Content[:])
 	output[SIZE-1] = t2.Salt
 	return output
+}
+
+// FromBytes reads a byte sequence ends with an even uint8 and retrieve the Salt.
+func (t1 *T1Message) FromBytes(data [SIZE]byte) {
+	if data[SIZE-1]%2 != 0 {
+		panic("Last byte must be even!")
+	}
+	t1.Salt = data[SIZE-1]
+}
+
+// FromBytes reads a byte sequence ends with an odd uint8 and retrieve the jobID, proxyID, Salt and Content.
+func (t2 *T2Message) FromBytes(data [SIZE]byte) {
+	if data[SIZE-1]%2 != 1 {
+		panic("Last byte must be odd!")
+	}
+	t2.JobID = binary.BigEndian.Uint32(data[0:4])
+	t2.ProxyID = binary.BigEndian.Uint32(data[4:8])
+	copy(t2.Content[:], data[8:SIZE-1])
+	t2.Salt = data[SIZE-1]
+}
+
+// GetType identifies the MessageType represented by data, which can be T1 or T2
+func GetType(data [SIZE]byte) MessageType {
+	lastByte := data[SIZE-1]
+	if lastByte%2 == 0 {
+		return T1
+	} else {
+		return T2
+	}
 }
