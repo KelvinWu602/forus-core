155181679582718180af876b635560d9c5045cb5
KelvinWu602 <wuhfhf@gmail.com>
2024-04-08 18:46:31 +0800

    debug msg will print out 1 more level of call stack

--- p2p/loggers.go
+++ p2p/loggers.go
@@ -3,16 +3,26 @@ package p2p
 import (
 	"log"
 	"net"
+	"runtime"
 )
 
+func getCallerName() string {
+	pc, _, _, ok := runtime.Caller(3)
+	details := runtime.FuncForPC(pc)
+	if ok && details != nil {
+		return details.Name()
+	}
+	return "unknown caller"
+}
+
 func logProtocolMessageHandlerError(handlerName string, tcpConn net.Conn, err error, input any) {
-	log.Printf("[%s@%s]:Error:%s\nInput:\n%v\n", handlerName, tcpConn.RemoteAddr().String(), err, input)
+	log.Printf("[%s.%s@%s]:Error:%s\nInput:\n%v\n", getCallerName(), handlerName, tcpConn.RemoteAddr().String(), err, input)
 }
 
 func logMsg(funcName string, msg string) {
-	log.Printf("[%s]:Log:%s\n", funcName, msg)
+	log.Printf("[%s.%s]:Log:%s\n", getCallerName(), funcName, msg)
 }
 
 func logError(funcName string, err error, description string) {
-	log.Printf("[%s]:Error:%s\n%s\n", funcName, description, err)
+	log.Printf("[%s.%s]:Error:%s\n%s\n", getCallerName(), funcName, description, err)
 }
